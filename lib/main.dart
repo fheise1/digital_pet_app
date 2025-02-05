7b2d9b994aad2d0f3a6f2c36d142417467f2c87d
fheise1 <flynnheise@gmail.com>
2025-02-05 18:40:48 -0500

    changed each tab name to represent a new animal and gave a brief description about them

--- lib/main.dart
+++ lib/main.dart
@@ -1,92 +1,93 @@
-import 'package:flutter/material.dart'; 
+import 'package:flutter/material.dart';
 
-void main() { 
-  runApp(MyApp()); 
-} 
+void main() {
+  runApp(MyApp());
+}
 
-class MyApp extends StatelessWidget { 
-  @override 
-  Widget build(BuildContext context) { 
-    return MaterialApp( //Basic setup. Can look at it as the default tab
-      home: DefaultTabController( //Manages the state for the tab interface. Each state will have something different
-        length: 4, 
-        child: _TabsNonScrollableDemo(), 
-      ), 
-    ); 
-  } 
-} 
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: DefaultTabController(
+        length: 4,
+        child: _TabsNonScrollableDemo(),
+      ),
+    );
+  }
+}
 
-class _TabsNonScrollableDemo extends StatefulWidget { //This is the stateful widget. It will hold the state/look of the tab
-  @override 
-  __TabsNonScrollableDemoState createState() => __TabsNonScrollableDemoState(); 
-} 
+class _TabsNonScrollableDemo extends StatefulWidget {
+  @override
+  __TabsNonScrollableDemoState createState() => __TabsNonScrollableDemoState();
+}
 
-class __TabsNonScrollableDemoState extends State<_TabsNonScrollableDemo> 
-    with SingleTickerProviderStateMixin, RestorationMixin { 
-  late TabController _tabController; 
+class __TabsNonScrollableDemoState extends State<_TabsNonScrollableDemo>
+    with SingleTickerProviderStateMixin, RestorationMixin {
+  late TabController _tabController;
 
-  final RestorableInt tabIndex = RestorableInt(0); //Restores initial tab when app is built
-  
-  @override 
-  String get restorationId => 'tab_non_scrollable_demo'; 
-
-  @override 
-  void restoreState(RestorationBucket? oldBucket, bool initialRestore) { 
-    registerForRestoration(tabIndex, 'tab_index'); 
-    _tabController.index = tabIndex.value; 
-  } 
-  
-  @override 
-  void initState() { 
-    super.initState(); 
-    _tabController = TabController( 
-      initialIndex: 0, 
-      length: 4, 
-      vsync: this, 
-    ); 
-    _tabController.addListener(() { 
-      setState(() { 
-        tabIndex.value = _tabController.index; 
-      }); 
-    }); 
-  } 
-
-  @override 
-  void dispose() { //Will remove the rab controller every time you remove a widget
-    _tabController.dispose(); 
-    tabIndex.dispose(); 
-    super.dispose(); 
-  } 
+  final RestorableInt tabIndex = RestorableInt(0);
 
   @override
-  Widget build(BuildContext context) { 
-// For the ToDo task hint: consider defining the widget and name of the tabs here 
-    final tabs = ['Tab 1.0', 'Tab 2.0', 'Tab 3.0', 'Tab4.0']; 
+  String get restorationId => 'tab_non_scrollable_demo';
 
-    return Scaffold( 
-      appBar: AppBar( 
-        automaticallyImplyLeading: false, 
-        title: Text( 
-    'Tabs Demo (Changed)', 
-        ), 
-        bottom: TabBar( 
-          controller: _tabController, 
-          isScrollable: false, 
-          tabs: [ 
-            for (final tab in tabs) Tab(text: tab), 
-          ], 
-        ), 
-      ), 
-      body: TabBarView( 
-        controller: _tabController, 
-        children: [ 
-// hint for the to do task:Considering creating the different for different tabs 
-          for (final tab in tabs) 
-            Center( 
-              child: Text(tab), 
-            ), 
-        ], 
-      ), 
-    ); 
-  } 
-} 
+  @override
+  void restoreState(RestorationBucket? oldBucket, bool initialRestore) {
+    registerForRestoration(tabIndex, 'tab_index');
+    _tabController.index = tabIndex.value;
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    _tabController = TabController(
+      initialIndex: 0,
+      length: 4,
+      vsync: this,
+    );
+    _tabController.addListener(() {
+      setState(() {
+        tabIndex.value = _tabController.index;
+      });
+    });
+  }
+
+  @override
+  void dispose() {
+    _tabController.dispose();
+    tabIndex.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    final pets = [
+      {'name': 'Buddy', 'description': 'Loves playing fetch.'},
+      {'name': 'Tom', 'description': 'Enjoys sitting on laps.'},
+      {'name': 'Swimmers', 'description': 'Swims around happily.'},
+      {'name': 'Charlie', 'description': 'Always greets at the door.'},
+    ];
+
+    return Scaffold(
+      appBar: AppBar(
+        automaticallyImplyLeading: false,
+        title: Text('Pet Tabs'),
+        bottom: TabBar(
+          controller: _tabController,
+          isScrollable: false,
+          tabs: [
+            for (final pet in pets) Tab(text: pet['name']),
+          ],
+        ),
+      ),
+      body: TabBarView(
+        controller: _tabController,
+        children: [
+          for (final pet in pets)
+            Center(
+              child: Text('${pet['name']}: ${pet['description']}'),
+            ),
+        ],
+      ),
+    );
+  }
+}
